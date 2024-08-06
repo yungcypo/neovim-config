0625c2c28ec878299b6c2aac410351b04f5ad607
cyprich <cypooriginal@gmail.com>
2024-08-06 10:26:25 +0200

    initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+    { 'wakatime/vim-wakatime', lazy = false },
+    { "nvim-telescope/telescope.nvim", dependencies = "tsakirist/telescope-lazy.nvim", lazy = false },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        lazy = false,
+        config = function()
+            require("refactoring").setup()
+        end,
+    },
+}
